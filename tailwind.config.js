6cccbac6d6582f5ec557f2f8aa7c404949bdb884
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-06-29 20:29:27 +0530

    add html & css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+
+      colors: {
+        "primary-400": "hsl(75, 94%, 57%)",
+
+        "neutral-100": "hsl(0, 0%, 100%)",
+        "neutral-400": "hsl(0, 0%, 20%)",
+        "neutral-700": "hsl(0, 0%, 12%)",
+        "neutral-900": "hsl(0, 0%, 8%)",
+      },
+
+      fontSize: {
+        sm: ["0.875rem", "1.5"],
+        "2xl": ["1.5rem", "1.5"],
+      },
+    },
   },
   plugins: [],
 }
